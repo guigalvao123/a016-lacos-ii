473af9af783bfa17ab4a3d4e7dd381bd2ac64b19
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-09-13 16:12:29 -0300

    Exercicios finalizados, e revisados, da aula a016-lacos-ii

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+let contador = 0
+
+for (pais of maioresPaises){
+    contador += 1
+    console.log(`${contador} - ${pais}`)
+}
